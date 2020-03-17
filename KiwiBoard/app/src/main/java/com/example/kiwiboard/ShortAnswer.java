7015d078d3aa63311d0c9ea6420ae31bce4bc77a
mstock64 <stockm0819@gmail.com>
2020-03-17 14:59:38 -0400

    #82
    Added basic functionality to page
    
    functionality includes:
      -Clear button now clears the answer textfield
      -Submit button now stores in answer and returns to main screen
      -Question description now is dynamic that changes on passed in question

--- KiwiBoard/app/src/main/java/com/example/kiwiboard/ShortAnswer.java
+++ KiwiBoard/app/src/main/java/com/example/kiwiboard/ShortAnswer.java
@@ -7,25 +7,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShortAnswer extends AppCompatActivity {
-    private EditText anwser_view;
+    /*
+    * Question question
+    * As the name implies this textfield will be used to hold the question being
+    * asked by professor
+    * */
+    private Question question;
+
+    /*
+     * EditText answer_view
+     * This EditText object will be used to manipulate the answer text field on the GUI xml
+     * */
+    private EditText answer_view;
+
+    /*
+     *  TextView txt_box
+     *  This TextView object will be used to manipulate the question text field on the GUI xml
+     * */
+    private TextView txt_box;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_short_answer);
+        if(question == null)
+            question = new Question(Question.QuestionType.SHORTANSWER,"What is 2 plus 2",null,0,64,10,10,0,null,null,0,null,null,0);
 
+        txt_box = findViewById(R.id.txt_question);
+        answer_view = findViewById(R.id.txt_anwser);
+        set_description();
     }
+
+    public void set_description(){ txt_box.setText(question.getDescription()); }
+    /*
+    *   public void switch_to_main(View view)
+    *   This function will be used switch from the short Activity and store the current students
+    *   answer in a question object
+    *
+     */
+
     public void switch_to_main(View view){
+        String answer = collect_answer();
+
+        Toast toast = Toast.makeText(getApplicationContext(),"Submitting Answer!",Toast.LENGTH_SHORT);
+        toast.setMargin(50,50);
+        toast.show();
+
+        System.out.println("Answer Collected:\t" + answer);
+        startActivity(new Intent(this, ProfMain.class));
+
+        question.setTextresponse(collect_answer());
+
         this.finish();
         startActivity(new Intent(ShortAnswer.this, StudentMain.class));
     }
-    //Credit: https://stackoverflow.com/questions/5308200/clear-text-in-edittext-when-entered used to derive line 26
-    public void clear_txt_anwser(View view){
-        anwser_view = (EditText) findViewById(R.id.txt_anwser);
-        try { anwser_view.getText().clear(); }
-        catch (Exception e){}
 
-    }
+
+    /*
+     *  public void clear_txt_anwser(View view)
+     *  This function will delete all text in the txt_answer
+     *
+     *  Credit: https://stackoverflow.com/questions/5308200/clear-text-in-edittext-when-entered
+     *  used to derive line 26
+     */
+
+    public void clear_txt_anwser(View view) { try { answer_view.getText().clear(); } catch (Exception e){} }
+
+    /*
+     *  public String collect_answer()
+     *  returns the text stored in txt_answer as a string
+     *
+     */
+    public String collect_answer(){ return findViewById(R.id.txt_anwser).toString(); }
+    public Question get_Question(){return question;}
+    public void set_Question(Question quest){ question = quest; }
 }
