93f106f3e863ec8d34225b8b18c60325af40ee79
bclarke <brandonbclarke@gmail.com>
2020-04-07 06:43:49 -0400

    #33
    - Implemented the timer on the Student page. Students can now see how much time is left to answer a question
    - edits to the circular progress bar XML and student multiple choice XML

--- KiwiBoard/app/src/main/java/com/example/kiwiboard/StudentMultipleChoice.java
+++ KiwiBoard/app/src/main/java/com/example/kiwiboard/StudentMultipleChoice.java
@@ -2,82 +2,124 @@ package com.example.kiwiboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class StudentMultipleChoice extends AppCompatActivity {
+    // Timer Variables
+    private static final long COUNTDOWN_IN_MILLIS = 60000;
+    private ColorStateList textColorDefaultCd;  // Want to change textColor when less than 10 secs remains
     private ProgressBar progressBar;
-    private static final long START_TIME_IN_MILLIS = 600000;
     private TextView timerText;
+    private RadioGroup radioGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button submitButton;
+    private ColorStateList textColorDefaultRb;
+
     private CountDownTimer countDownTimer;
-    private boolean timerRunning;
     private long timeLeftInMillis;
-    private long endTime;
+
+    private long backPressedTime;
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_multiple_choice);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.multChoice_toolbar);
         timerText = findViewById(R.id.txt_countdown);
-        setToolbar("Multiple Choice", toolbar);
+        radioGroup = findViewById(R.id.multipleChoiceOptions);
+        rb1 = findViewById(R.id.multchoice1);
+        rb2 = findViewById(R.id.multchoice2);
+        rb3 = findViewById(R.id.multchoice3);
+        submitButton = findViewById(R.id.submitButton);
+        progressBar = findViewById(R.id.progressBar);
 
-        this.progressBar = findViewById(R.id.progressBar);
+        textColorDefaultRb = rb1.getTextColors();
+        textColorDefaultCd = timerText.getTextColors();
+
+        setToolbar("Multiple Choice", toolbar);
         progressBar.setVisibility(View.VISIBLE);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //startTimer();
-                progressBar.setVisibility(View.GONE);
-            }
-        }, 200000);
+        // dbHelper is not coded yet
+        // immediately want to start timer when we display a new question
+        displayQuestion();
+    }
+
+    private void displayQuestion() {
+        //rb1.setTextColor(textColorDefaultRb);
+        //rb2.setTextColor(textColorDefaultRb);
+        //rb3.setTextColor(textColorDefaultRb);
+        //rb4.setTextColor(textColorDefaultRb);
+        radioGroup.clearCheck();
+
+        // code to set answer options will go here later
+
+        timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+        startTimer();
     }
 
     private void startTimer() {
-        endTime = System.currentTimeMillis() + timeLeftInMillis;
         countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timeLeftInMillis = millisUntilFinished;
                 updateCountDownText();
-                if(timeLeftInMillis == 0) {
-                    countDownTimer.cancel();
-                }
+                // need to set the percentage for the progress bar
+                // 1. Progress Bar only had 100 different settings. 0-100 integers
+                 int progress = (int) (100 * timeLeftInMillis / COUNTDOWN_IN_MILLIS);
+                 progressBar.setProgress(progress);
+
             }
             @Override
             public void onFinish() {
-                timerRunning = false;
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                countDownTimer.cancel();
+                progressBar.clearAnimation();
+                // checkAnswer will lock in the answer selected when time runs out
+                // when coded cancel the countDownTimer inside of it
+                // checkAnswer();
             }
         }.start();
-        timerRunning = true;
     }
 
     private void updateCountDownText() {
         int minutes = (int) (timeLeftInMillis / 1000) / 60;
-        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60; // only get remaining after dividing by 60
         // "%02d:%02d" : time string that looks like a clock
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-        timerText.setText(timeLeftFormatted);
+        setTimerText(timeLeftFormatted);
+
+        if(timeLeftInMillis < 10000) {
+            timerText.setTextColor(Color.RED);
+        }
+        else {
+            //timerText.setTextColor(textColorDefaultCd);
+        }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        SharedPreferences preferences = getSharedPreferences("prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        editor.putLong("millisLeft", timeLeftInMillis);
-        editor.putBoolean("timerRunning", timerRunning);
-        editor.putLong("endTime", endTime);
-        editor.apply();
+    protected void onDestroy() {
+        super.onDestroy();
 
         if(countDownTimer != null) {
             countDownTimer.cancel();
@@ -85,26 +127,15 @@ public class StudentMultipleChoice extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        SharedPreferences preferences = getSharedPreferences("prefs", MODE_PRIVATE);
-        timeLeftInMillis = preferences.getLong("millisLeft", START_TIME_IN_MILLIS);
-        timerRunning = preferences.getBoolean("timerRunning", false);
-        updateCountDownText();
-
-        if(timerRunning) {
-            endTime = preferences.getLong("endTime", 0);
-            timeLeftInMillis = endTime - System.currentTimeMillis();
-
-            if(timeLeftInMillis < 0) {
-                timeLeftInMillis = 0;
-                timerRunning = false;
-                updateCountDownText();
-            }
-            else
-                startTimer();
+    public void onBackPressed() {
+        if(backPressedTime + 2000 > System.currentTimeMillis()) {
+            finish();
         }
+        else {
+            Toast.makeText(this, "Press back twice quickly to finish",
+                    Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 
     public void setToolbar(String title, Toolbar toolbar) {
