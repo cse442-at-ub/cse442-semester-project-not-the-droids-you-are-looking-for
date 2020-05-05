82bc686055deffbd72d477b653c1fa7ae27bfe60
bclarke <brandonbclarke@gmail.com>
2020-05-05 16:46:52 -0400

    #140
    Deleted unnecessary comments

--- KiwiBoard/app/src/main/java/com/example/kiwiboard/StudentShortAnswer.java
+++ KiwiBoard/app/src/main/java/com/example/kiwiboard/StudentShortAnswer.java
@@ -190,15 +190,6 @@ public class StudentShortAnswer extends AppCompatActivity {
     }
 
 
-    //public void set_description(){ txt_box.setText(question.getDescription()); }
-    /*
-     *   public void switch_to_main(View view)
-     *   This function will be used switch from the short Activity and store the current students
-     *   answer in a question object
-     *
-     */
-
-
     public void switch_to_main(){
         String result = collect_answer();
         int index_c = StudentData.getCurrentcourse(), index_q = StudentData.getLastclickedquestion();
@@ -223,16 +214,7 @@ public class StudentShortAnswer extends AppCompatActivity {
     public void cover_switch_to_main(View view){
         switch_to_main();
     }
-
-    /*
-     *  public void clear_txt_anwser(View view)
-     *  This function will delete all text in the txt_answer
-     *
-     *  Credit: https://stackoverflow.com/questions/5308200/clear-text-in-edittext-when-entered
-     *  used to derive line 26
-
-    public void clear_txt_answer(View view) { try { answer_view.getText().clear(); } catch (Exception e){} }
-*/
+    
     // collect the text stored in txt_answer as a string
     public String collect_answer() { return answer.getText().toString();}
 
